116083ff612aab20e24eaf39f2e1a409d7e96ce9
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-04 22:22:38 -0400

    ReyesMilliam_Wilderick_Output_Assignment
    Declared and defined number variable

--- ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
+++ ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
@@ -2,8 +2,10 @@
  * Created by wilderickreyes on 6/4/15.
  */
 
-var carMakeAndModel;                //Declaration
-carMakeAndModel = "Audi A4";        //String Definition
+var carMakeAndModel;                //Declaration of string variable
+carMakeAndModel = "Audi A4";        //String variable Definition
 
 var isTheCarColorYellow = true;     //Declaration and definition of Boolean variable
 
+var manufacturingYear;              //Declaration of number variable
+manufacturingYear = 1998;           //Definition of number variable
