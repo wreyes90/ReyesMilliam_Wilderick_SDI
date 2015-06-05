a22229cf47c255798d7201a5bd9bcd988e755bb3
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-04 22:16:32 -0400

    ReyesMilliam_Wilderick_Output_Assignemnt
    Declared and defined Boolean variable

--- ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
+++ ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
@@ -2,5 +2,8 @@
  * Created by wilderickreyes on 6/4/15.
  */
 
-var carMakeAndModel;         //declaration
-carMakeAndModel = "Audi A4"; //Definition
+var carMakeAndModel;                //Declaration
+carMakeAndModel = "Audi A4";        //String Definition
+
+var isTheCarColorYellow = true;     //Declaration and definition of Boolean variable
+
