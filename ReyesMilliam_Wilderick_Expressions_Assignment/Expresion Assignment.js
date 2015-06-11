489314cff8c8c7754c486691ce849368b7ae2d65
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-10 21:42:20 -0400

    Expression Assignment 3
    Created the first part for the arrays portion. Assigned values to the
    variables on each room in order to get the area of each room, to later
    use the measurements to gather the whole surface area.

--- ReyesMilliam_Wilderick_Expressions_Assignment/Expresion Assignment.js
+++ ReyesMilliam_Wilderick_Expressions_Assignment/Expresion Assignment.js
@@ -10,3 +10,22 @@ console.log("Celsius",celsius);          // Run expression on the console to get
 var celsius1 = 20;                       // Assigned value to variable celsius1
 var fahrenheit1 = 9 / 5 * celsius1 + 32; // Assigned the expression to the variable fahrenheit1
 console.log("Fahrenheit",fahrenheit1);   // Run expression on on the console to get the converted temperature on fahrenheit
+
+// Using arrays to calculate the total square feet measurement of three rooms
+// Room 1
+var length1 = 8;                         // Assigned value to the variable length1
+var width1 = 10;                         // Assigned value to the variable width1
+var area1 = length1 * width1;            // Assigned area formula to variable area1
+console.log(area1, "Square feet");       // Run area formula on console to get the measurement of room 1
+
+// Room 2
+var length2 = 12;                        // Assigned value to the variable length2
+var width2 = 15;                         // Assigned value to the variable width2
+var area2 = length2 * width2;            // Assigned area formula to variable area2
+console.log(area2, "Square feet");       // Run area formula on console to get the measurement of room 2
+
+// Room 3
+var length3 = 12;                        // Assigned value to the variable length3
+var width3 = 8;                          // Assigned value to the variable width3
+var area3 = length3 * width3;            // Assigned area formula to variable area3
+console.log(area3, "Square feet");       // Run area formula on console to get the measurement of room 3
