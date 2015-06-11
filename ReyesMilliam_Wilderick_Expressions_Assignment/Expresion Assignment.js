fc06da53cb3c7731273e3d82e09a72d414214a50
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-10 20:28:14 -0400

    Expression Assignment 2
    - Assigned value to variable celsius1
    - Assigned mathematical expression on variable fahrenheit1
    - Ran expression on the console and got the converted temperature on
    Fahrenheit
    _Added descriptive comments

--- ReyesMilliam_Wilderick_Expressions_Assignment/Expresion Assignment.js
+++ ReyesMilliam_Wilderick_Expressions_Assignment/Expresion Assignment.js
@@ -1,11 +1,12 @@
 // Wilderick A. Reyes Milliam 06/11/2015
 // Fahrenheit to Celsius and Celsius to Fahrenheit calculations
 
-//Fahrenheit to Celsius
-var fahrenheit = 85;                     //Assigned a value to the variable Fahrenheit
-var celsius = 5 / 9 * (fahrenheit - 32); //Assigned the mathematical expression to the variable celsius
-console.log("Celsius",celsius);          //run expression on the console to get the converted temperature to celsius
+// Fahrenheit to Celsius
+var fahrenheit = 85;                     // Assigned a value to the variable Fahrenheit
+var celsius = 5 / 9 * (fahrenheit - 32); // Assigned the mathematical expression to the variable celsius
+console.log("Celsius",celsius);          // Run expression on the console to get the converted temperature on celsius
 
-
-
-
+// Celsius to Fahrenheit
+var celsius1 = 20;                       // Assigned value to variable celsius1
+var fahrenheit1 = 9 / 5 * celsius1 + 32; // Assigned the expression to the variable fahrenheit1
+console.log("Fahrenheit",fahrenheit1);   // Run expression on on the console to get the converted temperature on fahrenheit
