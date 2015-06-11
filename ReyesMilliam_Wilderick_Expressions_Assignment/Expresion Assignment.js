c5fa7cd9fac77546749224bacbe1be63d3ca4024
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-10 20:05:46 -0400

    Expression Assignment 1
    - Added the Tittle
    - Created variable fahrenheit and the variable celsius
    - Ran the expression on console and got the temperature in celsius
    (29.44 C)

---
+++ ReyesMilliam_Wilderick_Expressions_Assignment/Expresion Assignment.js
@@ -0,0 +1,11 @@
+// Wilderick A. Reyes Milliam 06/11/2015
+// Fahrenheit to Celsius and Celsius to Fahrenheit calculations
+
+//Fahrenheit to Celsius
+var fahrenheit = 85;                     //Assigned a value to the variable Fahrenheit
+var celsius = 5 / 9 * (fahrenheit - 32); //Assigned the mathematical expression to the variable celsius
+console.log("Celsius",celsius);          //run expression on the console to get the converted temperature to celsius
+
+
+
+
