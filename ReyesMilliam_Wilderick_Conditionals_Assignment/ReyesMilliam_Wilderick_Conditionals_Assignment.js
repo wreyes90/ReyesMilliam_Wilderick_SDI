0aeb127523b9f20a110398a20ac23c2d6e942f98
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-18 00:10:32 -0400

    Conditionals 1
    Set up several variables in the code. Entered the tittle.

---
+++ ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Conditionals_Assignment.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wilderickreyes on 6/17/15.
+ */
+// Wilderick A. Reyes Milliam
+// Miles Per Gallon Calculator
+
+// Assign variables
+
+var gallonsInTank = prompt("Input the ammount of gallons in gas tank");
+var distance = prompt ("Input the distance you are driving");
+var mpg = prompt ("Enter the aproximate MPG ecconomy of the vehicle")
+
