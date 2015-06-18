2b5628588d8c2e409dfe83f9e9bc65a373bbed50
wreyes90 <wareyesmilliam@fullsail.edu>
2015-06-18 03:10:33 -0400

    Conditional assignment 2
    Doubt with the info user provides on one of the prompts, asked
    professor a question

--- ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Conditionals_Assignment.js
+++ ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Conditionals_Assignment.js
@@ -6,7 +6,43 @@
 
 // Assign variables
 
-var gallonsInTank = prompt("Input the ammount of gallons in gas tank");
-var distance = prompt ("Input the distance you are driving");
-var mpg = prompt ("Enter the aproximate MPG ecconomy of the vehicle")
+var gallonsInTank = prompt ("Input the amount of gallons in gas tank:");
+var mpg = prompt ("Enter the approximate MPG economy of the vehicle:");
+var distance = false; prompt ("Input the distance you are driving in miles:"); // here is the doubt< I can not store the value the user enters here and use it on a new prompt or on the console
+distance = confirm("Is the distance in miles? Ok for yes, cancel for no");
+var range = (gallonsInTank * mpg);
 
+    if (distance === true) {
+
+    console.log("You have a range of " + range + " miles!");
+
+    }
+    else {
+    console.log("please try again and provided distance in miles.")
+    }
+
+var tripPossible = false;
+tripPossible = confirm ("is " + range + " grater than " + distance);
+
+        if (tripPossible === true) {
+    console.log ("Your range is " + range + " miles; for a total distance of " + distance + " miles trip is possible. Have a safe drive!")
+        }
+
+        else {
+    console.log ("Your range is only " + range + " miles; for a total distance of " + distance + " miles trip is not possible. Please refuel!")
+        }
+
+var convertToKM = false;
+convertToKM = confirm ("would you like to convert range and distance to Kilometers?")
+
+
+if (convertToKM === true) {
+    var convertedRange = range / 0.62137
+    var convertedDist = distance / 0.62137
+    console.log("Your range in Kilometers is: " + convertedRange + " KM and your converted distance is: " + convertedDist + " KM")
+
+}
+
+else {
+    console.log("Finished")
+}
