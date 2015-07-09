201aa38b505005ec2bc3e7dc2fcd5a903715590e
wreyes90 <wareyesmilliam@fullsail.edu>
2015-07-08 22:09:38 -0400

    Output Assignment 5
    Created the final console assignment, added comments describing each
    part of the code. Find code run was successful.

--- ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
+++ ReyesMilliam_Wilderick_Output_Assignment/ReyesMilliam_Wilderick_Output_Assignment.js
@@ -3,9 +3,9 @@
 // Output Assignment (where I work)
 
 // Declaring variables
-var jobPosition = "Bilingual Customer Service Representative";
-var yearsWorking = 3;
-var isCompanyATT = true;
+var jobPosition = "Bilingual Customer Service Representative"; // String type variable for assigning my job position
+var yearsWorking = 3;                                          // Number type variable for assigning the years I have been working.
+var isCompanyATT = true;                                       // Boolean Variable assigning true value to the company where I work.
 
-
-
+// Console
+console.log("I work for AT&T. \nMy position is " + jobPosition+". \nI have been working there for " + yearsWorking + " years."); // Assignment to the console the variables and concatenated the variables with string data.
