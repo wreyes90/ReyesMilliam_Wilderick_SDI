fb04f623e3d331aec7472068598da24217dd9e80
wreyes90 <wareyesmilliam@fullsail.edu>
2015-07-16 00:07:51 -0400

    Expressions Assignment 5
     Created a new variable to assign the total of the tip alone. Created
    new math expression to calculate the amount of the tip alone. Added
    comments to the code explaining each section. Assigned to the console
    the final result.

--- ReyesMilliam_Expressions_Assignment/ReyesMilliam_Wilderick_Expressions_Assignment.js
+++ ReyesMilliam_Expressions_Assignment/ReyesMilliam_Wilderick_Expressions_Assignment.js
@@ -3,15 +3,18 @@
 // Expressions Assignment (Tip Calculator)
 
 // Declaring Variables
-var tipPercentage;
-var total;
-var totalWithTip;
-
+var tipPercentage; // Variable for the percentage of the tip to be used.
+var total;         // Variable for the total of the purchase without the tip.
+var totalWithTip;  // Variable to declare the total of the purchase with the tip included.
+var totalOfTip;    // Variable for the total amount of the tip based on the total of the purchase and the tip percentage.
 
 // Use prompts to get user data.
-total         = Number(prompt("Enter the total amount of the purchase"));
-tipPercentage = Number(prompt("Enter the percentage of the tip"));
+total         = Number(prompt("Enter the total amount of the purchase")); // Prompt to get the users input of the total of the purchase.
+tipPercentage = Number(prompt("Enter the percentage of the tip"));        // Prompt to get the users input of the tip percentage.
 
 // Create the mathematical expression to get the correct value.
-totalWithTip = total * ((tipPercentage / 100) + 1);
+totalOfTip   = total * (tipPercentage / 100);                             // Mathematical expression to calculate the amount of the tip alone.
+totalWithTip = total * ((tipPercentage / 100) + 1);                       // Mathematical expression to calclate the total of the purchase with tip included.
 
+// Assignment to the console.
+console.log("The total of the tip is $" + totalOfTip+"\nThe total of your purchase with tip included is $" + totalWithTip); // Console assignment of the calculations concatenated with strings.
