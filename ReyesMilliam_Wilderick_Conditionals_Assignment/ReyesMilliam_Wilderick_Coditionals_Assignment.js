c04bac6840c99411456bea49c0f4d4125d569d9a
wreyes90 <wareyesmilliam@fullsail.edu>
2015-07-22 16:30:37 -0400

    Conditionals Assignment 2
    Created first conditionals of the code. Assigned outputs to the console
    for each outcome.

--- ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Coditionals_Assignment.js
+++ ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Coditionals_Assignment.js
@@ -8,3 +8,10 @@ var totalOfPurchase = Number(prompt("Enter the total amount of your purchase."))
 var salesTax        = Number(prompt("Enter the percentage of the sales tax in your area."));
 var purchaseWithTax = totalOfPurchase + (totalOfPurchase * (salesTax/100));
 var moneyNeeded     = purchaseWithTax - myBalance;
+
+//Conditionals Assignment
+if (myBalance >= purchaseWithTax) {
+    console.log("Your balance is $" + myBalance + " and the total of your purchase with taxes is $" + purchaseWithTax + " you can make the purchase.");
+} else {
+    console.log("You do not have enough balance to make the purchase. You need $" + moneyNeeded + " to make the purchase.");
+}
