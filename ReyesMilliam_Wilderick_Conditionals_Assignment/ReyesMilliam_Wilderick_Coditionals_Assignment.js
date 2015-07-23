7626e62834eef8e116e1fab782fd8df14d86bfac
wreyes90 <wareyesmilliam@fullsail.edu>
2015-07-22 23:34:41 -0400

    Conditional Assignment 5
    Created a ternary on the code and improved the validation process by
    adding logical operators.

--- ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Coditionals_Assignment.js
+++ ReyesMilliam_Wilderick_Conditionals_Assignment/ReyesMilliam_Wilderick_Coditionals_Assignment.js
@@ -10,18 +10,19 @@ var purchaseWithTax = totalOfPurchase + (totalOfPurchase * (salesTax/100));
 var moneyNeeded     = purchaseWithTax - myBalance;
 
 //Conditionals Assignment
-if (myBalance == "") {
-    alert("Account balance not provided \nPlease enter your account balance");
+if (myBalance == "" || 0) {
+    alert("Account balance not provided or is equal to 0 \nPlease enter your account balance");
     }
-    if (totalOfPurchase == ""){
-        alert("The total amount of the purchase not provided \nPlease enter the total amount of your purchase.");
+    if (totalOfPurchase == "" || 0){
+        alert("The total amount of the purchase not provided or is equal to 0 \nPlease enter the total amount of your purchase.");
     }
     if (salesTax == ""){
         alert("The sales tax was not entered \nPlease enter the sales tax");
     }
-else if (myBalance >= purchaseWithTax){
-     console.log("Your balance is $" + myBalance + " and the total of your purchase with tax included is $" + purchaseWithTax + "\nPurchase is possible");
-    }
-    else {
-    console.log("You do not have enough balance to make the purchase. You need $" + moneyNeeded + " to make the purchase.");
-}
+//else if (myBalance >= purchaseWithTax){
+     //console.log("Your balance is $" + myBalance + " and the total of your purchase with tax included is $" + purchaseWithTax + "\nPurchase is possible");
+    //}
+    //else {
+    //console.log("You do not have enough balance to make the purchase. You need $" + moneyNeeded + " to make the purchase.");
+//}
+myBalance >= purchaseWithTax ? console.log("Your balance is $" + myBalance + " and the total of your purchase with tax included is $" + purchaseWithTax + "\nPurchase is possible") : console.log("You do not have enough balance to make the purchase. You need $" + moneyNeeded + " to make the purchase.");
