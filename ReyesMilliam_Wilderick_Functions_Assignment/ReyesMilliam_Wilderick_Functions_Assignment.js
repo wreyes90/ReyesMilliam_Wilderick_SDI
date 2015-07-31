9dbe6f1e586d1e186ced4a2eab740e117deddf81
wreyes90 <wareyesmilliam@fullsail.edu>
2015-07-30 22:48:29 -0400

    Functions Assignment Flowchart
    Created the flowchart for the code

--- ReyesMilliam_Wilderick_Functions_Assignment/ReyesMilliam_Wilderick_Functions_Assignment.js
+++ ReyesMilliam_Wilderick_Functions_Assignment/ReyesMilliam_Wilderick_Functions_Assignment.js
@@ -10,15 +10,15 @@ var pbFinalNum; // Variable to assigne the random final number of the Power Ball
 
 // Lottery selection and assignment function
 function lottery(lotteryType, flNumbers, pbNumbers, pbFinal) { // lottery function with the internal variables set as parameters
-    var clicks = 1; // internat variable used for the validation if the correct info was typed on the prompt
+    var clicks = 1; // internal variable used for the validation if the correct info was typed on the prompt
 
     while (lotteryType === "") { // while loop used to validate correct info was typed by the user.
 
         lotteryType = prompt("Please make sure you chose between:\nFlorida Lottery or Power Ball"); // Prompt to be shown if no or if incorrect info was typed.
 
-        clicks ++; // math expresion for the clicks variable.
+        clicks ++; // math expression for the clicks variable.
 
-        if (clicks === 4) { // if statemnt to set up a limit on how many times to loop will run.
+        if (clicks === 4) { // if statement to set up a limit on how many times to loop will run.
             console.log("Maximum number of attempts reached.\nPlease reload the page and try again."); // Assignment to the console if the stament is true
             break; // Brake for the code to stop and not continue the loop.
         }
@@ -28,7 +28,7 @@ function lottery(lotteryType, flNumbers, pbNumbers, pbFinal) { // lottery functi
         console.log("The Florida Lottery winning numbers are:\n" + flNumbers ); // Assignment to the console the message to show if Florida Lottery was selected concatenated with the random numbers.
     }
     if (lotteryType === "Power Ball") { // if Statement to run if Power Ball was chosen.
-        console.log("The Power Ball winning numbers are:\n" + pbNumbers + " PB " + pbFinal); // Assignemnt to the conso of the message if Power Bal was typed concatenated with the random numbers.
+        console.log("The Power Ball winning numbers are:\n" + pbNumbers + " PB " + pbFinal); // Assignment to the console of the message if Power Bal was typed concatenated with the random numbers.
     }
 }
 
